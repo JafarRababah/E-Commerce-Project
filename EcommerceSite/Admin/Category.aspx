cb333e1bbdbc40ecaeb382e77cf5529c9f342df1
JafarRababah <j3fr.rababah@gmail.com>
2024-05-26 13:26:32 +0300

    Add All Shop online Project Files

---
+++ EcommerceSite/Admin/Category.aspx
@@ -0,0 +1,130 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="Category.aspx.cs" Inherits="EcommerceSite.Admin.Category" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script>
+        window.onload = function () {
+            var seconds = 5;
+            setTimeout(function () {
+                document.getElementById("<%#lblMsg.ClientID%>").style.display = "none";
+            }, seconds * 1000);
+        };
+    </script>
+    <script>
+        function ImagePreview(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#<%=imagePreview.ClientID%>').prop('src', e.target.result)
+                        .width(200)
+                        .height(200);
+                };
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="mb-4">
+        <asp:Label ID="lblMsg" runat="server" ></asp:Label>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">
+                        Category
+                    </h4><hr>
+                    <div class="form-body">
+                        <label>Category Name</label>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <asp:TextBox ID="txtCategoryName" CssClass="form-control" placeholder="Enter Category Name" runat="server"></asp:TextBox>
+                                    <asp:RequiredFieldValidator ID="rfvCategoryName" ForeColor="Red" Font-Size="Small" Display="Dynamic" SetFocusOnError="true" runat="server" ErrorMessage="Required" ControlToValidate="txtCategoryName"></asp:RequiredFieldValidator>
+                                </div>
+                            </div>
+                        </div>
+                         <label>Category Image</label>
+                        <div class="row">
+                          <div class="col-sm-12">
+                               <div class="form-group">
+                                   <asp:FileUpload ID="fuCategoryImage" CssClass="form-control" runat="server"
+                                       onchange="ImagePreview(this);"/>
+                                   <asp:HiddenField ID="hfCategoryID" runat="server" Value="0" />
+                                </div>
+                             </div>
+                           </div>
+                        <div class="=row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <asp:CheckBox ID="cbIsActive" runat="server" Text="&nbsp; IsActive" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-actiona pb-5">
+                        <div class="text-left">
+                            <asp:Button ID="btnAddOrUpdate" runat="server" CssClass="btn btn-info" Text="Add" OnClick="btnAddOrUpdate_Click" CausesValidation="True" />
+                            <asp:Button ID="btnClear" runat="server" CssClass="btn btn-dark" Text="Reset" OnClick="btnClear_Click" CausesValidation="True" />
+                            </div>
+                    </div>
+                    <div>
+                        <asp:Image ID="imagePreview" CssClass="img-thumbnail" runat="server" />
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-8">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">
+                Category List
+            </h4><hr/>
+            <div class="table-responsive">
+                <asp:Repeater ID="rCategory" runat="server" onItemCommand="rCategory_ItemCommand">
+                <HeaderTemplate>
+                    <table class="table datatable-export table-hover nowrap">
+                        <thead>
+                            <tr>
+                                <th class="table-plus">Name</th>
+                                <th >Image</th>
+                                <th >IsActive</th>
+                                <th >CreateDate</th>
+                                <th class="datatable-nosort">Action</th>
+                            </tr>
+                        </thead>
+                    <tbody>
+                    </HeaderTemplate>
+                    <ItemTemplate>
+                        <tr>
+                        <td class="table-plus"> <%# Eval("CategoryName") %> </td>
+                        <td> 
+                            <img width="40" src="<%# EcommerceSite.clsUtils.GetImageUrl(Eval("CategoryImage")) %>" alt="image" />
+                        </td>
+                            <td>
+                                <asp:Label ID="lblIsActive" runat="server" Text='<%#(bool)Eval("IsActive")==true?"Active":"In-Active" %>' 
+                                    CssClass='<%#(bool)Eval("IsActive")==true?"badge badge-success":"badge badge-danger" %>'></asp:Label>
+                            </td>
+                            <td> <%# Eval("CreateDate") %></td>
+                              <td>  <asp:LinkButton ID="lbEdit" runat="server" CssClass="badge badge-primary"
+                                  CommandArgument='<%# Eval("CategoryID") %>' CommandName="edit" CausesValidation="False"><i class="fas fa-edit"></i></asp:LinkButton>
+                                   <asp:LinkButton ID="lbDelete" runat="server" CssClass="badge badge-danger" 
+                                       CommandArgument='<%# Eval("CategoryID") %>' CommandName="delete" CausesValidation="False"><i class="fas fa-trash-alt"></i></asp:LinkButton>
+
+                            
+                           
+
+                            </td>
+                         </tr>
+                    </ItemTemplate>
+                   <FooterTemplate>
+                       </tbody>
+                    </table>
+                   </FooterTemplate>
+                    </asp:Repeater>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+</asp:Content>
---
+++ EcommerceSite/Admin/Category.aspx.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace EcommerceSite.Admin
+{
+
+    
+
+    public partial class Category : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        SqlDataReader reader;
+        DataTable tbl;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["breakCumbTitle"] = "Manager Category";
+            Session["breakCumbPage"] = "Category";
+            lblMsg.Visible = false;
+            GetCategories();
+        }
+        protected void rCategory_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            lblMsg.Visible=false;
+            if (e.CommandName == "edit")
+            {
+                con = new SqlConnection(clsUtils.GetConnection());
+                cmd = new SqlCommand("sp_Category", con);
+                cmd.Parameters.AddWithValue("@Action", "GetByID");
+                cmd.Parameters.AddWithValue("@CategoryID", e.CommandArgument);
+                cmd.CommandType = CommandType.StoredProcedure;
+                adapter = new SqlDataAdapter(cmd);
+                tbl = new DataTable();
+                adapter.Fill(tbl);
+                txtCategoryName.Text = tbl.Rows[0]["CategoryName"].ToString();
+                cbIsActive.Checked = Convert.ToBoolean(tbl.Rows[0]["IsActive"]);
+                imagePreview.ImageUrl = string.IsNullOrEmpty(tbl.Rows[0]["CategoryImage"].ToString()) ? "../Images/No_image.png" : "../" + tbl.Rows[0]["CategoryImage"].ToString();
+                imagePreview.Height = 200;
+                imagePreview.Width = 200;
+                hfCategoryID.Value = tbl.Rows[0]["CategoryID"].ToString();
+                btnAddOrUpdate.Text = "Update";
+            }
+            else if (e.CommandName == "delete")
+            {
+                con = new SqlConnection(clsUtils.GetConnection());
+                cmd = new SqlCommand("sp_Category", con);
+                cmd.Parameters.AddWithValue("@Action", "Delete");
+                cmd.Parameters.AddWithValue("@CategoryID", e.CommandArgument);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Category Deleted Successfully";
+                    lblMsg.CssClass = "alert alert-success";
+                    GetCategories();
+                    Clear();
+                }
+                catch (Exception ex)
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Error: " + ex.Message;
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+                finally { con.Close(); }
+            }
+        }
+        void Clear()
+        {
+            txtCategoryName.Text = string.Empty;
+            cbIsActive.Checked= false;
+            hfCategoryID.Value = "0";
+            btnAddOrUpdate.Text = "Add";
+            imagePreview.ImageUrl = string.Empty;
+        }
+        void GetCategories()
+        {
+            con = new SqlConnection(clsUtils.GetConnection());
+            cmd = new SqlCommand("sp_Category", con);
+            cmd.Parameters.AddWithValue("@Action", "GetAll");
+            cmd.CommandType = CommandType.StoredProcedure;
+            adapter=new SqlDataAdapter(cmd);
+            tbl = new DataTable();
+            adapter.Fill(tbl);
+            rCategory.DataSource = tbl;
+            rCategory.DataBind();
+        }
+        protected void btnAddOrUpdate_Click(object sender, EventArgs e)
+        {
+            string ActionName= string.Empty, ImagePath = string.Empty,FileExtention=string.Empty;
+            bool IsValidExecute= false;
+            int CategoryID=Convert.ToInt32(hfCategoryID.Value);
+            con = new SqlConnection(clsUtils.GetConnection());
+            cmd = new SqlCommand("sp_Category", con);
+            cmd.Parameters.AddWithValue("@Action", CategoryID == 0 ? "Insert" : "Update");
+            cmd.Parameters.AddWithValue("@CategoryID", CategoryID);
+            cmd.Parameters.AddWithValue("@CategoryName", txtCategoryName.Text);
+            cmd.Parameters.AddWithValue("@IsActive", cbIsActive.Checked);
+            if(fuCategoryImage.HasFile)
+            {
+                if (clsUtils.IsValidExtention(fuCategoryImage.FileName))
+                    {
+                    string newImageName = clsUtils.GetUniqueID();
+                    FileExtention = Path.GetExtension(fuCategoryImage.FileName);
+                    ImagePath = "Images/Category/" + newImageName.ToString() + FileExtention;
+                    fuCategoryImage.PostedFile.SaveAs(Server.MapPath("~/Images/Category/") + newImageName.ToString() + FileExtention); 
+                    cmd.Parameters.AddWithValue("@CategoryImageUrl",ImagePath);
+                    IsValidExecute = true;
+                }
+                else
+                {
+                    lblMsg.Visible = false;
+                    lblMsg.Text = "Please Select .jpg, .png or .jpeg images";
+                    lblMsg.CssClass = "alert alert-danger";
+                    IsValidExecute = false;
+                }
+                
+            }
+            else
+            {
+                IsValidExecute = true;
+            }
+            if (IsValidExecute)
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    ActionName = CategoryID == 0 ? "Inserted" : "Updated";
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Category " + ActionName + " Successfully";
+                    lblMsg.CssClass = "alert alert-success";
+                    GetCategories();
+                    Clear();
+                }
+                catch (Exception ex) {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Error: " + ex.Message;
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+                finally { con.Close(); }
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+        
+    }
+}

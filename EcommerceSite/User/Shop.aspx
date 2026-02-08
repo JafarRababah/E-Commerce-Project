d84c19cda67759792035547b4b4f9bdc4e6ab2de
JafarRababah <j3fr.rababah@gmail.com>
2026-02-08 18:38:45 +0300

    Modify shop page search button

--- EcommerceSite/User/Shop.aspx
+++ EcommerceSite/User/Shop.aspx
@@ -202,18 +202,20 @@
                         <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
                             <div class="card product-item border-0 mb-4">
                                 <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                    <img class="img-fluid w-100" src="../UserTemplate/img/product-1.jpg" alt="">
+                                    <img class="img-fluid w-100" src='<%# EcommerceSite.clsUtils.GetImageUrl(Eval("ImageURL")) %>' alt="">
                                 </div>
                                 <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                                    <h6 class="text-truncate mb-3">Colorful Stylish Shirt</h6>
+                                    <h6 class="text-truncate mb-3"> <%# Eval("ShortDescription") %></h6>
                                     <div class="d-flex justify-content-center">
-                                        <h6>$123.00</h6>
+                                        <h6> <%# Eval("Price") %></h6>
                                         <h6 class="text-muted ml-2"><del>$123.00</del></h6>
                                     </div>
                                 </div>
                                 <div class="card-footer d-flex justify-content-between bg-light border">
-                                    <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
-                                    <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                                    <a href=" <%# "ShopDetail.aspx?id="+ Eval("ProductID") %>" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
+                                    <asp:LinkButton ID="lbAddCart" runat="server" CssClass="btn btn-sm text-dark p-0" CommandName="addToCart" CommandArgument='<%# Eval("ProductID") %>'>
+                                        <i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart
+                                    </asp:LinkButton>
                                 </div>
                             </div>
                         </div></ItemTemplate>
--- EcommerceSite/User/Shop.aspx.cs
+++ EcommerceSite/User/Shop.aspx.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +12,99 @@ namespace EcommerceSite.User
 {
     public partial class Shop : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        DataTable tbl;
+        DataView dv;
+        clsUtils utils;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                getAllProducts();
+            }
+        }
+        void getAllProducts()
+        {
+            try
+            {
+                using (con = new SqlConnection(clsUtils.GetConnection()))
+                {
+                    con.Open();
+                    cmd = new SqlCommand("sp_Product", con);
+                    cmd.Parameters.AddWithValue("@Action", "ActiveProduct");
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    adapter = new SqlDataAdapter(cmd);
+                    tbl = new DataTable();
+                    adapter.Fill(tbl);
+                    if (tbl.Rows.Count > 0)
+                    {
+                        rProducts.DataSource = tbl;
+                    }
+                    else
+                    {
+                        rProducts.DataSource = tbl;
+                        rProducts.FooterTemplate = null;
+                        rProducts.FooterTemplate=new CustomTemlate(ListItemType.Footer);
+                    }
+                    rProducts.DataBind();
+                    Session["product"] = tbl;
+                }
 
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('"+ ex.Message+ "')</script>");
+            }
+        }
+        private sealed class CustomTemlate : ITemplate
+        {
+            private ListItemType ListItemType {  get; set; }
+            public CustomTemlate(ListItemType listItemType)
+            {
+                ListItemType = listItemType;
+            }
+
+            public void InstantiateIn(Control container)
+            {
+                if (ListItemType == ListItemType.Footer)
+                {
+                    var footer = new LiteralControl("<b>No Product da desplay,</b>");
+                    container.Controls.Add(footer);
+                    
+                }
+            }
+        }
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            tbl = (DataTable)Session["product"];
+            if(tbl!=null)
+            {
+                if (tbl.Rows.Count > 0)
+                {
+                    dv = new DataView(tbl);
+                    var search = txtSearchInput.Value.Trim().Replace("'", "''");
+                    dv.RowFilter = $"ShortDescription LIKE '%{search}%'";
+                    if (dv.Count > 0)
+                    {
+                        rProducts.DataSource = dv;
+                    }
+                    else
+                    {
+                        rProducts.DataSource = dv;
+                        rProducts.FooterTemplate = null;
+                        rProducts.FooterTemplate=new CustomTemlate(ListItemType.Footer);
+                    }
+                }
+                else
+                {
+                    rProducts.DataSource = dv;
+                    rProducts.FooterTemplate = null;
+                    rProducts.FooterTemplate = new CustomTemlate(ListItemType.Footer);
+                }
+                rProducts.DataBind();
+            }
         }
     }
 }
